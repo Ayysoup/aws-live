71b2fc58846ea9a2af59c9e68fd47a528238893a
Ayysoup <114733137+Ayysoup@users.noreply.github.com>
2024-04-16 14:46:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c9geiigo6xts.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "thongyucheng-employee"
+customregion = "us-east-1"
+
